e53e27daf0fd6bb5e21bebe12e845b3dca6d5ee5
Parth <201322006+ParthShendge@users.noreply.github.com>
2025-09-25 17:28:37 +0530

    Change folder structure,  add react router to project and install tailwind dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-import './styles/middleSection.css'
-import './styles/topSection.css'
-import './styles/MessageBox.css'
-import TopSection from './components/TopSection'
-import MiddleSection from './components/MiddleSection'
-import BottomSection from './components/BottomSection'
-import SideBar from './components/SideBar'
+import { Link } from 'react-router-dom'
+import Chatbot from './pages/Chatbot/Chatbot'
 
 function App() {
-  const [messages, setMessages] = useState([{type:'bot', message:'Hi User', key:'message1'},
-                                            {type:'bot', message:'How can i help you?', key:'message2'},
-                                            {type:'user', message:'Help me find fees payment page.', key:'message3'},
-                                            {type:'bot', message:'Here you go....', key:'message4'}]);
   
   return (
     <>
-     <TopSection />
-     <MiddleSection messages={messages}/>
-     <BottomSection setMessages={setMessages} messages={messages}/>
-     <SideBar />
+      <p>Hello, this is our project</p>
+      <Link to='/chatbot'>chatbot</Link>
+      <p>Hello, this is our project</p>
     </>
   )
 }
